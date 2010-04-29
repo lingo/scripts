c8bd199955e3cb28c1d154b56cb0af263483a33a
Luke Hudson <lukeletters@gmail.com>
2010-04-30 10:22:30 +1200

    MINOR: Tweak sed usage

--- jardump.sh
+++ jardump.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
 JAR="$1"
-SED="gsed"
+SED="sed"
+
+# Try to guess which sed tool
+sed -r  >/dev/null 2>&1 
+if [ $? -ne 0 ]; then
+	SED="gsed"
+fi
+
 jar tf "$JAR" | $SED -r 's/\//./g; /\.$/d; /META/d; s/\.class$//g; /\$/d' | xargs javap -classpath "$JAR"
