0fe90951e014ed3e7f519c571cca25113b3b8614
Luke Hudson <lukeletters@gmail.com>
2010-04-29 15:42:43 -0700

    Added argument checking

--- jardump.sh
+++ jardump.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# Usage:  jardump.sh  <file.jar>
+# Lists classes and their method signatures and public properties.
+#
+
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 </path/to/file>"
+	exit 1
+fi 
 
 JAR="$1"
 SED="sed"
